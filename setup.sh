8e53792614877de42b5d94e86bbe35ab5ddbcf3d
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2024-06-10 16:55:06 -0300

    :sparkles: improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-# configure the Kibana password in the ES container
-# curl -u elastic:elastic \
-# 	-X POST \
-# 	http://localhost:9200/_security/user/kibana_system/_password \
-# 	-d '{"password":"'"kibana"'"}' \
-# 	-H 'Content-Type: application/json'
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/kibana_system/_password \
+	-d '{"password":"'"kibana"'"}' \
+	-H 'Content-Type: application/json'
 
-# curl -u elastic:elastic \
-# 	-X POST \
-# 	http://localhost:9200/_security/user/logstash_system/_password \
-# 	-d '{"password":"'"logstash"'"}' \
-# 	-H 'Content-Type: application/json'
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/role/logstash_writer \
+	-d '{
+        "cluster": ["manage_index_templates", "monitor", "manage_ilm"], 
+        "indices": [
+          {
+            "names": [ "logstash-*" ], 
+            "privileges": ["write","create","create_index","manage","manage_ilm"]  
+          }
+        ]
+  }' \
+	-H 'Content-Type: application/json'
+
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/logstash_internal \
+	-d '{
+        "password" : "logstash",
+        "roles" : [ "logstash_writer"],
+        "full_name" : "Internal Logstash User"
+  }' \
+	-H 'Content-Type: application/json'
 
 # curl -u elastic:elastic \
 # 	-X POST \
@@ -19,8 +36,8 @@
 # 	-d '{"password":"'"heartbeat"'"}' \
 # 	-H 'Content-Type: application/json'
 
-curl -u elastic:elastic \
-	-X POST \
-	http://localhost:9200/_security/user/beats_system/_password \
-	-d '{"password":"'"heartbeat"'"}' \
-	-H 'Content-Type: application/json'
+# curl -u elastic:elastic \
+# 	-X POST \
+# 	http://localhost:9200/_security/user/beats_system/_password \
+# 	-d '{"password":"'"heartbeat"'"}' \
+# 	-H 'Content-Type: application/json'

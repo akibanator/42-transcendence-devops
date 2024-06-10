c6c9bc953571a293fcc45f859de6a63a828553c7
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2024-06-10 13:31:25 -0300

    :sparkles: unfinished base structure

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# configure the Kibana password in the ES container
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/kibana_system/_password \
+	-d '{"password":"'"kibana"'"}' \
+	-H 'Content-Type: application/json'
+
+# curl -u elastic:elastic \
+# 	-X POST \
+# 	http://localhost:9200/_security/user/logstash_system/_password \
+# 	-d '{"password":"'"logstash"'"}' \
+# 	-H 'Content-Type: application/json'
+
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/beats_system/_password \
+	-d '{"password":"'"heartbeat"'"}' \
+	-H 'Content-Type: application/json'

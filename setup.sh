5cacdf6efd3edbccd5d4a3d8de745efcf01d7511
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2024-06-11 11:57:46 -0300

    :sparkles: improve setup.sh

--- setup.sh
+++ setup.sh
@@ -30,14 +30,46 @@ curl -u elastic:elastic \
   }' \
 	-H 'Content-Type: application/json'
 
-# curl -u elastic:elastic \
-# 	-X POST \
-# 	http://localhost:9200/_security/user/beats_system/_password \
-# 	-d '{"password":"'"heartbeat"'"}' \
-# 	-H 'Content-Type: application/json'
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/beats_system/_password \
+	-d '{"password":"'"heartbeat"'"}' \
+	-H 'Content-Type: application/json'
 
-# curl -u elastic:elastic \
-# 	-X POST \
-# 	http://localhost:9200/_security/user/beats_system/_password \
-# 	-d '{"password":"'"heartbeat"'"}' \
-# 	-H 'Content-Type: application/json'
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/role/heartbeat_writer \
+	-d '{
+        "cluster": ["monitor", "manage_ilm"], 
+        "indices": [
+          {
+            "names": [ "heartbeat-*" ], 
+            "privileges": ["create_doc"]  
+          }
+        ]
+  }' \
+	-H 'Content-Type: application/json'
+
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/role/heartbeat_reader \
+	-d '{
+        "cluster": ["monitor", "manage_ilm"], 
+        "indices": [
+          {
+            "names": [ "heartbeat-*" ], 
+            "privileges": ["read"]  
+          }
+        ]
+  }' \
+	-H 'Content-Type: application/json'
+
+curl -u elastic:elastic \
+	-X POST \
+	http://localhost:9200/_security/user/heartbeat_internal \
+	-d '{
+        "password" : "heartbeat",
+        "roles" : [ "kibana_admin","ingest_admin","monitoring_user","heartbeat_writer"],
+        "full_name" : "Internal Heartbeat User"
+  }' \
+	-H 'Content-Type: application/json'
